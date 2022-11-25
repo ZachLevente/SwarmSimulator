4fd5d87e4f5b1f31cde258974f5c9fe3c7ebe4ab
ZachLevente <zach.levente@gmail.com>
2022-11-25 23:52:23 +0100

    Update Bird Controller

--- SwarmSimulator/Assets/Scripts/Monobehaviours/BirdObjectController.cs
+++ SwarmSimulator/Assets/Scripts/Monobehaviours/BirdObjectController.cs
@@ -1,9 +1,36 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Something.Controllers
 {
     public class BirdObjectController : MonoBehaviour
     {
-        public Entity Brain { get; set; }
+        public Entity Brain { get; private set; }
+        
+        private static List<BirdObjectController> _birds = new();
+
+        public BirdObjectController Spawn(Entity entity)
+        {
+            Brain = entity;
+            Move();
+            return this;
+        }
+        
+        public void Move()
+        {
+            transform.position = GameManager.Instance.WorldSpaceGridController.GetGameWorldPosition(Brain.Position);
+            //TODO rotation
+        }
+        
+        public static void MoveAllBirds() => _birds.ForEach(b => b.Move());
+
+        #region Callbacks
+
+        private void OnEnable() => _birds.Add(this);
+
+        private void OnDisable() => _birds.Remove(this);
+
+        #endregion
     }
 }
