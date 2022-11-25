dd7fdf344021c2a26d067d43710456cf50378ea8
ZachLevente <zach.levente@gmail.com>
2022-11-25 20:52:08 +0100

    Add ui prefab

--- SwarmSimulator/Assets/Scripts/Monobehaviours/WorldSpaceGridController.cs
+++ SwarmSimulator/Assets/Scripts/Monobehaviours/WorldSpaceGridController.cs
@@ -9,19 +9,35 @@ namespace Something.Controllers
         [SerializeField] private Vector3Int createdGridSize = new Vector3Int(5, 5, 5);
         private WorldSpaceGrid GridModel;
         
-        private void Start()
+        #region Public Metohds
+
+        public void CreateStandardSizeGrid()
         {
-            CreateGrid(createdGridSize.x, createdGridSize.y, createdGridSize.z);
+            KillAllChildren();
+            CreateGrid(createdGridSize.x, createdGridSize.y, new System.Random().Next(5));
         }
+
+        #endregion
+
+        #region Private Implementation
 
         private void CreateGrid(int x, int y, int z)
         {
             GridModel = new WorldSpaceGrid(x, y, z);
 
             for (int i = 0; i < x; i++)
-                for (int j = 0; j < y; j++)
-                    for (int k = 0; k < z; k++)
-                        Instantiate(gridDotPrefab, new Vector3(i, j, k), Quaternion.identity, gameObject.transform);
+            for (int j = 0; j < y; j++)
+            for (int k = 0; k < z; k++)
+                Instantiate(gridDotPrefab, new Vector3(i, j, k), Quaternion.identity, gameObject.transform);
         }
+
+        private void KillAllChildren()
+        {
+            foreach (Transform child in transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        #endregion
     }
 }
