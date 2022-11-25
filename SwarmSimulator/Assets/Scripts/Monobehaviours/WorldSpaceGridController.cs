a1038d2cf774bafa6bd44a9f1eb02e0256fa2d8e
ZachLevente <zach.levente@gmail.com>
2022-11-25 23:54:14 +0100

    Update WorldSpaceGridController

--- SwarmSimulator/Assets/Scripts/Monobehaviours/WorldSpaceGridController.cs
+++ SwarmSimulator/Assets/Scripts/Monobehaviours/WorldSpaceGridController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Something.Controllers
@@ -5,17 +6,37 @@ namespace Something.Controllers
     public class WorldSpaceGridController : MonoBehaviour
     {
         [SerializeField] private GameObject gridDotPrefab;
+        [SerializeField] private BirdObjectController birdPrefab;
         
-        [SerializeField] private Vector3Int createdGridSize = new Vector3Int(5, 5, 5);
-        private WorldSpaceGrid GridModel;
-        
+        [SerializeField] private Vector3Int createdGridSize = new Vector3Int(50, 50, 50);
+        private WorldSpaceGrid _gridModel;
+        private GameObject[,,] _dots;
+
         #region Public Metohds
 
-        public void CreateStandardSizeGrid()
+        public WorldSpaceGrid GetGrid()
+        {
+            return _gridModel;
+        }
+
+        public Vector3 GetGameWorldPosition(Vector3Int gridPos)
+        {
+            return _dots[gridPos.x, gridPos.y, gridPos.z].transform.position;
+        }
+
+        public void CreateNewStandardSizeGrid()
         {
             transform.KillAllChildren();
-            CreateGrid(createdGridSize.x, createdGridSize.y, new System.Random().Next(5));
+            CreateGrid(createdGridSize.x, createdGridSize.y, createdGridSize.z);
         }
+
+        public BirdObjectController AddEntity(Entity entity)
+        {
+            _gridModel.AddEntity(entity);
+            return Instantiate(birdPrefab, transform).Spawn(entity);
+        }
+        
+        
 
         #endregion
 
@@ -23,12 +44,13 @@ namespace Something.Controllers
 
         private void CreateGrid(int x, int y, int z)
         {
-            GridModel = new WorldSpaceGrid(x, y, z);
+            _dots = new GameObject[x, y, z];
+            _gridModel = new WorldSpaceGrid(x, y, z);
 
             for (int i = 0; i < x; i++)
             for (int j = 0; j < y; j++)
             for (int k = 0; k < z; k++)
-                Instantiate(gridDotPrefab, new Vector3(i, j, k), Quaternion.identity, gameObject.transform);
+                _dots[i,j,k] = Instantiate(gridDotPrefab, new Vector3(i, j, k), Quaternion.identity, gameObject.transform);
         }
 
         #endregion
