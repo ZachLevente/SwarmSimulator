df1ecb3b7fc9585c5e5d8ba8fbf548a7a5e98bc5
ZachLevente <zach.levente@gmail.com>
2022-11-25 23:55:17 +0100

    Update WorldSpaceGrid model

--- SwarmSimulator/Assets/Scripts/models/WorldSpaceGrid.cs
+++ SwarmSimulator/Assets/Scripts/models/WorldSpaceGrid.cs
@@ -1,34 +1,43 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Something
 {
     [Serializable]
     public class WorldSpaceGrid
     {
-        [SerializeField] private Field[,,] fields;
-        [SerializeField] private List<Entity> entities = new List<Entity>();
+        [SerializeField] private Field[,,] _fields;
+        [SerializeField] private List<Entity> _entities = new List<Entity>();
+
+        //TODO should be a read-only copy
+        public Field[,,] Fields => _fields;
 
         internal WorldSpaceGrid(int x, int y, int z) {
-            fields = new Field[x,y,z];
+            _fields = new Field[x,y,z];
             for (int i = 0; i < x; i++)
                 for (int j = 0; j < y; j++)
                     for (int k = 0; k < z; k++)
-                        fields[i,j,k] = new Field();
+                        _fields[i,j,k] = new Field();
         }
 
         internal void step()
         {
-            foreach (var entity in entities)
-                entity.selectDestination(fields);
-            foreach (var entity in entities)
-                entity.stepIfAble(fields);
+            foreach (var entity in _entities)
+                entity.selectDestination(_fields);
+            foreach (var entity in _entities)
+                entity.stepIfAble(_fields);
         }
 
         internal void reset()
         {
             
+        }
+
+        internal void AddEntity(Entity entity)
+        {
+            _entities.Add(entity);
         }
     }
 }
