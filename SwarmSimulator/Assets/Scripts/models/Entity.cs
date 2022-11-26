618f9be624056dcda27d4abd13527c009486055c
ZachLevente <zach.levente@gmail.com>
2022-11-26 15:52:55 +0100

    Add setters to entity

--- SwarmSimulator/Assets/Scripts/models/Entity.cs
+++ SwarmSimulator/Assets/Scripts/models/Entity.cs
@@ -8,9 +8,17 @@ namespace Something
     {
         [SerializeField] protected Vector3Int _position;
         [SerializeField] protected Vector3 _direction;
-        
-        public Vector3Int Position => _position;
-        public Vector3 Direction => _direction;
+
+        public Vector3Int Position 
+        { 
+            get => _position;
+            set => _position = value;
+        }
+        public Vector3 Direction
+        {
+            get => _direction;
+            set => _direction = value;
+        }
 
         internal abstract void SelectDestination(Field[,,] env);
 
