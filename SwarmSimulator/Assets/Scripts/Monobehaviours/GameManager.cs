c5e672849dece6c1587e5d197865be2cc9c3fe2a
ZachLevente <zach.levente@gmail.com>
2022-11-26 16:47:23 +0100

    Save bird pos modifications

--- SwarmSimulator/Assets/Scripts/Monobehaviours/GameManager.cs
+++ SwarmSimulator/Assets/Scripts/Monobehaviours/GameManager.cs
@@ -5,17 +5,17 @@ namespace Something.Controllers
 {
     public class GameManager : MonoBehaviour
     {
-        private static GameManager _instance;
         public static GameManager Instance => _instance;
+        private static GameManager _instance;
 
-        private WorldSpaceGridController _worldSpaceGridController;
         public WorldSpaceGridController WorldSpaceGridController => _worldSpaceGridController;
+        private WorldSpaceGridController _worldSpaceGridController;
 
-        private GameUpdateController _gameTimeController;
         public GameUpdateController GameTimeController => _gameTimeController;
+        private GameUpdateController _gameTimeController;
 
-        private UiController _uiController;
         public UiController UiController => _uiController;
+        private UiController _uiController;
 
         private void Start()
         {
