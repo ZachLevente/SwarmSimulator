a02309676f8a93815b02d4b4eb6bc3d4f8965785
ZachLevente <zach.levente@gmail.com>
2022-11-27 12:52:45 +0100

    Camera rotation

--- SwarmSimulator/Assets/Scripts/Monobehaviours/CameraMovement.cs
+++ SwarmSimulator/Assets/Scripts/Monobehaviours/CameraMovement.cs
@@ -6,104 +6,39 @@ namespace Something.Controllers
 {
     public class CameraMovement : MonoBehaviour
     {
-        /*private Camera cam;
-        public Transform cam_parent;
-
-        //the x and y axis values of the controller or the WSAD
-        float horizontal, vertical;
-        public float Cam_speed;
-        private Quaternion m_camRot, m_Cam_parentRot;
-        [SerializeField] [Range(1, 8)] private float MouseXSens = 2f;
-
-        [SerializeField] [Range(1, 8)] private float MouseYSens = 2f;
-
-        // Use this for initialization
-        [SerializeField] private bool HideTheCursor;
-
-        void Start()
-        {
-            cam = GetComponent<Camera>();
-            m_camRot = transform.localRotation;
-            m_Cam_parentRot = cam_parent.localRotation;
-            if (HideTheCursor)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetButton(“Fire2”))
-            {
-                SetRotation();
-                MoveThecamera();
-            }
-        }
-
-        //For setting up the rotation of camera with respect to the mouse position
-        void SetRotation()
-        {
-            //multiplying the current rotation with the input axis values
-            m_camRot = m_camRot * Quaternion.Euler(-Input.GetAxis(“Mouse Y”) * MouseXSens,0f,0f);
-            m_Cam_parentRot = m_Cam_parentRot * Quaternion.Euler(0f, Input.GetAxis(“Mouse X”) * MouseYSens,0f);
-            
-            //setting the localRotation values to the desired rotation values
-            transform.localRotation = m_camRot;
-            cam_parent.localRotation = m_Cam_parentRot;
-        }
-
-        void MoveThecamera()
-        {
-            //taking horizpontal input into the horizontal variable
-            horizontal = Input.GetAxis(“Horizontal”);
-            
-            //changing the parent position based on the camera’s transform.right
-            //transform.right also consider rotation too
-            //so that it can move in the ‘right’ direction
-            cam_parent.transform.position += cam.transform.right * horizontal * Cam_speed * Time.deltaTime;
-            
-            //taking vertical axis into vertical variable
-            vertical = Input.GetAxis(“Vertical”);
-
-            //changing the parent position based on the camera’s        transform.forward
-            //transform is also 0,0,1 but it also consider rotation too
-            //so that it can move in the forward direction considering the  rotation too
-            cam_parent.transform.position += cam.transform.forward * vertical * Cam_speed * Time.deltaTime;
-        }*/
-
         private float _cameraSpeed = 0.1f;
+        private float _panSpeed = 0.7f;
 
-        private void Start()
-        {
-            
-        }
+        private Transform _transform;
 
         private void Update()
         {
+            _transform = transform;
             MoveCamera();
+            RotateCamera();
+            transform.position = _transform.position;
+            transform.rotation = _transform.rotation;
         }
 
         private void MoveCamera()
         {
-            var pos = transform.position;
+            var pos = _transform.position;
             
             if (Input.GetKey(KeyCode.W))
             {
-                pos += transform.forward * _cameraSpeed;
+                pos += _transform.forward * _cameraSpeed;
             }
             if (Input.GetKey(KeyCode.S))
             {
-                pos -= transform.forward * _cameraSpeed;
+                pos -= _transform.forward * _cameraSpeed;
             }
             if (Input.GetKey(KeyCode.A))
             {
-                pos -= transform.right * _cameraSpeed;
+                pos -= _transform.right * _cameraSpeed;
             }
             if (Input.GetKey(KeyCode.D))
             {
-                pos += transform.right * _cameraSpeed;
+                pos += _transform.right * _cameraSpeed;
             }
             if (Input.GetKey(KeyCode.Space))
             {
@@ -114,7 +49,15 @@ namespace Something.Controllers
                 pos += Vector3.down * _cameraSpeed;
             }
 
-            transform.position = pos;
+            _transform.position = pos;
+        }
+
+        private void RotateCamera()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                _transform.eulerAngles += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * _panSpeed;
+            }
         }
     }
 }
