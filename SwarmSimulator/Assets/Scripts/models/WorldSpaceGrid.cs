51fe8081d622db7b79915db13596c54280af18db
ZachLevente <zach.levente@gmail.com>
2022-11-27 17:38:53 +0100

    Add FindEmptyPosition method to WorldSpaceGrid

--- SwarmSimulator/Assets/Scripts/models/WorldSpaceGrid.cs
+++ SwarmSimulator/Assets/Scripts/models/WorldSpaceGrid.cs
@@ -1,30 +1,47 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Something
 {
     public class WorldSpaceGrid
     {
-        private readonly Entity[,,] _field;
+        private readonly Entity[,,] _fields;
         private readonly List<Entity> _entities = new();
 
-        public Entity[,,] Fields => _field;
+        public Entity[,,] Fields => _fields;
 
         public WorldSpaceGrid(int x, int y, int z)
         {
-            _field = new Entity[x,y,z];
+            _fields = new Entity[x,y,z];
         }
 
-        internal void Step()
+        public void Step()
         {
             foreach (var entity in _entities)
-                entity.SelectDestination(_field, _entities);
+                entity.SelectDestination(_fields, _entities);
             foreach (var entity in _entities)
-                entity.StepIfAble(_field);
+                entity.StepIfAble(_fields);
         }
 
-        internal void AddEntity(Entity entity)
+        public void AddEntity(Entity entity)
         {
             _entities.Add(entity);
+        }
+        
+        public List<Vector3Int> FindEmptyPositions()
+        {
+            List<Vector3Int> results = new List<Vector3Int>();
+            var xlen = _fields.GetLength(0);
+            var ylen = _fields.GetLength(1);
+            var zlen = _fields.GetLength(2);
+            
+            for (int i = 0; i < xlen; i++)
+            for (int j = 0; j < ylen; j++)
+            for (int k = 0; k < zlen; k++)
+                if (_fields[i, j, k] == null)
+                    results.Add(new Vector3Int(i, j, k));
+            
+            return results;
         }
     }
 }
